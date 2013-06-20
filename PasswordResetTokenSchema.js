48f7c993ca8d9c32e1d4a74dbbc509c94a32d621
Austin Mullins <austin.mullins@gmail.com>
2013-06-19 23:23:22 -0500

    created PasswordResetTokenSchema.js

---
+++ PasswordResetTokenSchema.js
@@ -0,0 +1,14 @@
+(function() {
+    "user strict";
+
+    var mongoose = require("mongoose"),
+    ObjectId = mongoose.Schema.ObjectId;
+
+    var PasswordResetTokenSchema = new mongoose.Schema({
+        user: {type: ObjectId, ref: "User"},
+        token: {type: String, required: true},
+        expiry: {type: Date, required: true}
+    });
+
+    module.exports = PasswordResetTokenSchema;
+}).call(this);
